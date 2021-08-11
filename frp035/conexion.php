27db03ce5ffa58e3285fcbd977fc1274678ba370
JosueYP <josueyepezponce@hotmail.com>
2021-08-10 19:57:01 -0500

    puse un HOLA MUNDO en el archivo cn.php

--- frp035/conexion.php
+++ frp035/conexion.php
@@ -1,6 +1,7 @@
 
 <?php
 $mysqli = new PDO("mysql:host=127.0.0.1:3306; dbname=frp035", "root", "root");
+echo "hola mundo";
 
 //$mysqli = new PDO("mysql:host=localhost; dbname=ujmfcdbxvpnax", "tow4ubd4u4ym", "dbmbdsjwowlgux");
 
